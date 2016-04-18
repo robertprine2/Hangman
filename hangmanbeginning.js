303d71370aa26ed2ea42d89ec2c0221713585c97
Robert Prine <robertprine2@yahoo.com>
2016-04-18 00:40:37 -0400

    added images made html and attempted to make js since I don't know how to put them together

---
+++ hangmanbeginning.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Brains: Zombie Chase Hangman Style</title>
+</head>
+<body>
+	<script>
+
+// array for possible hangman words
+
+var words = ["ankle biters", "infected", "roamers", "risen", "living dead", "walking dead", "undead", "walkers", "corpse", "death", "disembody", "disembowel", "monster", "nightmare", "brains", "survival", "molotov cocktail", "traumatize", "crowbar", "machete", "tourniquet", "cadaverous", "fester", "armageddon"]
+
+// press any key to start
+
+//pick a word at random
+
+
+
+//fill in spaces
+
+// Number of guesses user gets based on body parts in current zombie chasing
+
+// press letter key to guess it - user key events
+
+// if it is in the word fill it in plus scream/get away from me sound effect at random
+
+// if it is not put it under letters guessed and cut off a limb plus sound effect
+
+// if it has already been guessed prompt user for another guess
+
+// If you get the word, you get brains and plays sound clip of a zombie saying "mmm brains"
+
+//automatically picks a new word and adds a win tally or loss tally
+
+
+
+	</script>
+</body>
+</html>
